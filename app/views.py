da1d3aab6cdfa140ac1fe0a714f1eec20cde6ce8
srivani143 <polukasanthosh573@gmail.com>
2024-03-20 06:46:39 -0700

    first commit

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def allu(request):
+    return render(request,'allu.html')
+
+def priy(request):
+    return render(request,'priy.html')
+
+def allu2(request):
+    return render(request,'allu2.html')
